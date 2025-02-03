5c0203f218bfd7756892cf6e16a801ca3a5140f2
abbas <abbas22013@gmail.com>
2025-02-03 20:46:25 +0300

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,35 @@
-// src/App.jsx
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { useState } from 'react'; 
-import './App.css';
-import IngredientList from './components/IngredientList/IngredientList'; 
-import BurgerStack from './components/BurgerStack/BurgerStack'; 
-
-const App = () => {
-  const availableIngredients = [
-    { name: 'Kaiser Bun', color: 'saddlebrown' },
-    { name: 'Sesame Bun', color: 'sandybrown' },
-    { name: 'Gluten Free Bun', color: 'peru' },
-    { name: 'Lettuce Wrap', color: 'olivedrab' },
-    { name: 'Beef Patty', color: '#3F250B' },
-    { name: 'Soy Patty', color: '#3F250B' },
-    { name: 'Black Bean Patty', color: '#3F250B' },
-    { name: 'Chicken Patty', color: 'burlywood' },
-    { name: 'Lettuce', color: 'lawngreen' },
-    { name: 'Tomato', color: 'tomato' },
-    { name: 'Bacon', color: 'maroon' },
-    { name: 'Onion', color: 'lightyellow' },
-    { name: 'Cheddar Cheese', color: '#FDE18B' },
-    { name: 'Swiss Cheese', color: '#F1E1A8' },
-  ];
-
-  // حالة لتخزين المكونات التي تم إضافتها للبرغر
-  const [stack, setStack] = useState([]);
-
- // Function to add ingredient
-  const addToBurger = (ingredient) => {
-    setStack([...stack, ingredient]);
-  };
-
- // Function to remove ingredient
-  const removeFromBurger = () => {
-    // نستخدم pop لإزالة آخر عنصر من المصفوفة
-    const newStack = [...stack]; // ننسخ المصفوفة الأصلية لتجنب تعديلها مباشرة
-    newStack.pop(); // نزيل آخر عنصر في المصفوفة
-    setStack(newStack); // نحدّث حالة البرغر
-  };
-  
-  
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <main>
-      <h1>Burger Stacker</h1>
-      <section>
-      <IngredientList ingredients={availableIngredients} onAddToBurger={addToBurger} />
-      <BurgerStack ingredients={stack} onRemoveFromBurger={removeFromBurger} />
-      </section>
-    </main>
-  );
-};
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
